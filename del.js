24e77c31303a3916b7b6047e3b8df3cdeaf589f6
Mishutin Dmitrii <47987074+MeShootIn@users.noreply.github.com>
2019-07-11 17:57:17 +0500

    Delete del.js

--- del.js
+++
@@ -1,6 +0,0 @@
-let del_button = document.getElementById("list_content").getElementsByClassName("ui_actions_menu_item");
-for(i = 0; i < del_button.length; ++i){
-	if(del_button[i].innerHTML == "Убрать из друзей"){
-		del_button[i].click();
-	}
-}
