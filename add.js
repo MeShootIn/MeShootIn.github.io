fb4cb5bf2df8b5837089e5cd2cea6cbb39a7b1e3
Mishutin Dmitrii <47987074+MeShootIn@users.noreply.github.com>
2019-07-11 15:46:28 +0300

    Add files via upload

--- add.js
+++ add.js
@@ -1,4 +1,4 @@
-let add = document.getElementsByClassName("flat_button button_small");
-for(i = 0; i < add.length; i += 2){
-	add[i].click();
+let add_button = document.getElementsByClassName("flat_button button_small");
+for(i = 0; i < add_button.length; i += 2){
+	add_button[i].click();
 }
