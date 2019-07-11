fee4c06fa5315c2f0c0f6c6e79bec9ecefb37e83
Mishutin Dmitrii <47987074+MeShootIn@users.noreply.github.com>
2019-07-11 16:35:01 +0300

    Add files via upload

--- vk_scripts.js
+++ vk_scripts.js
@@ -6,10 +6,19 @@ function add_friends(){
 }
 
 function del_friends(){
-	let del_button = document.getElementById("list_content").getElementsByClassName("ui_actions_menu_item");
+	let del_button = document.getElementsByClassName("ui_actions_menu_item");
 	for(i = 0; i < del_button.length; ++i){
 		if(del_button[i].innerHTML == "Убрать из друзей"){
 			del_button[i].click();
+		}
+	}
+}
+
+function clear_wall(){
+	let del_record_button = document.getElementsByClassName("ui_actions_menu_item");
+	for(i = 0; i < del_record_button.length; ++i){
+		if(del_record_button[i].innerHTML == "Удалить запись"){
+			del_record_button[i].click();
 		}
 	}
 }
