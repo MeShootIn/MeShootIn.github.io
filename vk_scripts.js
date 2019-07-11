a8ef30e06e6852240f41267613ac49d87e373d00
Mishutin Dmitrii <47987074+MeShootIn@users.noreply.github.com>
2019-07-11 16:21:45 +0300

    Add files via upload

--- vk_scripts.js
+++ vk_scripts.js
@@ -1,19 +1,15 @@
-class Script{
-	add(){
-		let add_button = document.getElementsByClassName("flat_button button_small");
-		for(i = 0; i < add_button.length; i += 2){
-			add_button[i].click();
-		}
-	}
-
-	del(){
-		let del_button = document.getElementById("list_content").getElementsByClassName("ui_actions_menu_item");
-		for(i = 0; i < del_button.length; ++i){
-			if(del_button[i].innerHTML == "Убрать из друзей"){
-				del_button[i].click();
-			}
-		}
+function add_friends(){
+	let add_button = document.getElementsByClassName("flat_button button_small");
+	for(i = 0; i < add_button.length; i += 2){
+		add_button[i].click();
 	}
 }
 
-let cmd = new Script();
+function del_friends(){
+	let del_button = document.getElementById("list_content").getElementsByClassName("ui_actions_menu_item");
+	for(i = 0; i < del_button.length; ++i){
+		if(del_button[i].innerHTML == "Убрать из друзей"){
+			del_button[i].click();
+		}
+	}
+}
