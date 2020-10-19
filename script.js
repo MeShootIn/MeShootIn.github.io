3e5fa034bf27a91a9c6305f32e3fb837257f8ed2
MeShootIn <dmitriimishutin@gmail.com>
2020-10-20 01:53:55 +0300

    Fixed bug with parsing url params

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 window.onload = () => {
     const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
 
-    if (urlParams.has('url')) {
-        location.href = urlParams.get('url');
-    }
+    location.href = queryString.substring(5);
 };
